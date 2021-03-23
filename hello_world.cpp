bc16d345549c4bbaa0f91352ed5a2b1ac819dc36
Mike Ofori <61565391+monk63@users.noreply.github.com>
2021-03-23 23:56:13 +0000

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char * argv[]){
+    if (argc <= 1){
+        cout << "Please specify at least one input value" << endl;
+        cout << "Example: hello_world Michael" << endl;
+        cerr << "Program Terminating" << endl;
+    }
+    else{
+        cout << "hello world " << argv[1]<< endl;
+        return 0;
+    }
+    
+
+
+}
